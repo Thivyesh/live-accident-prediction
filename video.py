e434a89ce9020260fca966708736ef4f977e0f53
Thivyesh Ahilathasan <a.thivy@gmail.com>
2024-12-27 16:35:06 +0100

    refactor: :recycle: remove unused code

--- video.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load an official or custom model
-model = YOLO("yolo11n.pt")  # Load an official Detect model
-
-# Perform tracking with the model
-results = model.track("https://www.youtube.com/watch?v=H9QSGqioYww", show=True, tracker="bytetrack.yaml")  # with ByteTrack
